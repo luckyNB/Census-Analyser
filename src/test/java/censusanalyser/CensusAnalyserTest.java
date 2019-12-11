15cbc445aac8c672c16b0c1baa71fcbb0ee06110
luckyNB <20150773@dbatu.ac.in>
2019-12-11 15:53:16 +0530

    Added: testcase when delimeter of csv file is wrong or not given should throw exception

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -11,7 +11,8 @@ public class CensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private static final String INDIA_STATTE_CODE_CSV_FILE_PATH = "/home/admin1/Downloads/CensusAnalyser/CensusAnalyser/src/test/resources/IndiaStateCode.csv";
-private static final String INDIA_STATE_CENSUS_WRONG="/home/admin1/Downloads/CensusAnalyser/CensusAnalyser/src/test/resources/IndiaStateCensusDataWrong.csv";
+    private static final String INDIA_STATE_CENSUS_WRONG = "/home/admin1/Downloads/CensusAnalyser/CensusAnalyser/src/test/resources/IndiaStateCensusDataWrong.csv";
+
     @Test
     public void givenIndianCodeCSVFile_WhenCorrect_Should_ReturnNumberOfRecordAvailable() {
         try {
@@ -22,8 +23,16 @@ private static final String INDIA_STATE_CENSUS_WRONG="/home/admin1/Downloads/Cen
         }
     }
 
-
-
+    @Test
+    public void givenIndianCensusCSVFile_When_IncorrectDelimeteter_ShouldThrowCensusAnalyserException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_STATE_CENSUS_WRONG);
+            Assert.assertEquals(29, numOfRecords);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.WRONG_DELIMETER_OR_HEADER, e.type);
+        }
+    }
 
     @Test
     public void givenIndianCensusData_WhenWrongFile_Should_ThrowCensusAnalyserException() {
@@ -37,20 +46,18 @@ private static final String INDIA_STATE_CENSUS_WRONG="/home/admin1/Downloads/Cen
         }
     }
 
-
     @Test
     public void givenIndianCensusCSVFile_WhenCorrect_Should_ReturnsNumberOfRecord() {
         CensusAnalyser censusAnalyser = new CensusAnalyser();
         ExpectedException exceptionRule = ExpectedException.none();
         int numOfRecords = 0;
         try {
-            numOfRecords=censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
             censusAnalyser.loadIndiaStateCodeData(INDIA_STATTE_CODE_CSV_FILE_PATH);
             Assert.assertEquals(29, numOfRecords);
         } catch (CensusAnalyserException e) {
             e.printStackTrace();
         }
-
     }
 
     @Test
@@ -58,15 +65,13 @@ private static final String INDIA_STATE_CENSUS_WRONG="/home/admin1/Downloads/Cen
         CensusAnalyser censusAnalyser = new CensusAnalyser();
         String sortedCensusData = null;
         try {
-        censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
             censusAnalyser.loadIndiaStateCodeData(INDIA_STATTE_CODE_CSV_FILE_PATH);
-            sortedCensusData=censusAnalyser.getStateWiseSortedCensusData();
+            sortedCensusData = censusAnalyser.getStateWiseSortedCensusData();
             IndiaCensusCSV[] indiaCensusCSV = new Gson().fromJson(sortedCensusData, IndiaCensusCSV[].class);
             Assert.assertEquals("Andhra Pradesh", indiaCensusCSV[0].state.trim());
         } catch (CensusAnalyserException | CSVBuilderException e) {
             e.printStackTrace();
         }
     }
-
-
 }
