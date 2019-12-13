7baf448725b45481c8fa8115c319a8c49cf1f6bd
luckyNB <20150773@dbatu.ac.in>
2019-12-13 17:55:43 +0530

    Added test-case sorted US Census Data by density then Alaska and Dictrict of Columbia should be first and last respectively

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -20,7 +20,7 @@ public class CensusAnalyserTest {
         IndiaCensusAdapter indiaCensusAdapter = new IndiaCensusAdapter();
         try {
             CensusAnalyser censusAnalyser = new CensusAnalyser();
-            Map<String, CensusDAO> result = indiaCensusAdapter.loadingCensusData(CensusAnalyser.Country.INDIA, INDIA_CENSUS_CSV_FILE_PATH);
+            Map<String, CensusDAO> result = indiaCensusAdapter.loadingCensusData(INDIA_CENSUS_CSV_FILE_PATH);
             Assert.assertEquals(29, result.size());
         } catch (CensusAnalyserException e) {
 
@@ -31,7 +31,7 @@ public class CensusAnalyserTest {
     public void givenStateCensusData_WhenWrongFileType_ThenShould_ThrowCensusAnalyserException() {
         IndiaCensusAdapter indiaCensusAdapter = new IndiaCensusAdapter();
         try {
-            Map<String, CensusDAO> result = indiaCensusAdapter.loadingCensusData(CensusAnalyser.Country.INDIA, WRONG_CSV_FILE_PATH);
+            Map<String, CensusDAO> result = indiaCensusAdapter.loadingCensusData(WRONG_CSV_FILE_PATH);
             Assert.assertEquals(29, result.size());
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
@@ -42,7 +42,7 @@ public class CensusAnalyserTest {
     public void givenStateCensusData_WhenWrongDelimeter_ThenShould_ThrowCensusAnalyserException() {
         IndiaCensusAdapter indiaCensusAdapter = new IndiaCensusAdapter();
         try {
-            Map<String, CensusDAO> result = indiaCensusAdapter.loadingCensusData(CensusAnalyser.Country.INDIA, INDIA_STATE_CENSUS_WRONG);
+            Map<String, CensusDAO> result = indiaCensusAdapter.loadingCensusData(INDIA_STATE_CENSUS_WRONG);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.WRONG_DELIMETER_OR_HEADER, e.type);
         }
@@ -157,7 +157,7 @@ public class CensusAnalyserTest {
     public void givenUSCensusData_WhenCorrect_Should_ReturnValidNumberOfRecords() {
         USCensusAdapter usCensusAdapter = AdapterFactory.getAdapterObject(CensusAnalyser.Country.USA);
         try {
-            Map<String, CensusDAO> result = usCensusAdapter.loadingCensusData(CensusAnalyser.Country.USA, US_CENSUS_DATA);
+            Map<String, CensusDAO> result = usCensusAdapter.loadingCensusData(US_CENSUS_DATA);
             Assert.assertEquals(51, result.size());
         } catch (CensusAnalyserException e) {
             e.printStackTrace();
@@ -168,7 +168,7 @@ public class CensusAnalyserTest {
     public void givenUSCensusData_WhenIncorrectDelimeter_Then_Should_ThrowCensusAnalyserException() {
         USCensusAdapter usCensusAdapter = AdapterFactory.getAdapterObject(CensusAnalyser.Country.USA);
         try {
-            Map<String, CensusDAO> result = usCensusAdapter.loadingCensusData(CensusAnalyser.Country.USA, US_CENSUS_DATA_WRONG_DATA);
+            Map<String, CensusDAO> result = usCensusAdapter.loadingCensusData(US_CENSUS_DATA_WRONG_DATA);
             Assert.assertEquals(51, result.size());
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.WRONG_DELIMETER_OR_HEADER, e.type);
@@ -179,7 +179,7 @@ public class CensusAnalyserTest {
     public void givenUSCensusData_WhenWrongHeader_ThenShould_ThrowCensusAnalyserException() {
         USCensusAdapter usCensusAdapter = AdapterFactory.getAdapterObject(CensusAnalyser.Country.USA);
         try {
-            Map<String, CensusDAO> result = usCensusAdapter.loadingCensusData(CensusAnalyser.Country.USA, WRONGFILETYPE);
+            Map<String, CensusDAO> result = usCensusAdapter.loadingCensusData(WRONGFILETYPE);
             Assert.assertEquals(51, result.size());
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.WRONG_DELIMETER_OR_HEADER, e.type);
@@ -236,6 +236,30 @@ public class CensusAnalyserTest {
         }
     }
 
+    @Test
+    public void givenUSCensusData_WhenSortedByDensity_ThenAlaska_ShouldBeFirstState() {
+        CensusAnalyser censusAnalyser=new CensusAnalyser();
+        try {
+            censusAnalyser.loadCensusData(CensusAnalyser.Country.USA,US_CENSUS_DATA);
+            String sortedDataIndiaCensusCSV = censusAnalyser.getSortedCensusCSV(FieldName.DENSITY, CensusAnalyser.Country.USA);
+            USCensusData[] usCensusData=new Gson().fromJson(sortedDataIndiaCensusCSV,USCensusData[].class);
+            Assert.assertEquals("Alaska",usCensusData[0].state.trim());
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void givenUSCensusData_WhenSortedByDensity_ThenDistrict_of_Columbia_ShouldBeLastState() {
+        CensusAnalyser censusAnalyser=new CensusAnalyser();
+        try {
+            censusAnalyser.loadCensusData(CensusAnalyser.Country.USA,US_CENSUS_DATA);
+            String sortedDataIndiaCensusCSV = censusAnalyser.getSortedCensusCSV(FieldName.DENSITY, CensusAnalyser.Country.USA);
+            USCensusData[] usCensusData=new Gson().fromJson(sortedDataIndiaCensusCSV,USCensusData[].class);
+            Assert.assertEquals("District of Columbia",usCensusData[usCensusData.length-1].state.trim());
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 
